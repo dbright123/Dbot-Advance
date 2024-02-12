4a3c5a995729a014bbc1f29846144d933f9e13cd
OMAGE MICHEAL BRIGHT <micheal.omage@gmail.com>
2024-02-12 16:26:36 +0100

    on it

--- dbot_news.py
+++ dbot_news.py
@@ -28,39 +28,67 @@ while True:
 
     currency = ['USDCAD','USDJPY','EURUSD','GBPUSD','XAUUSD','AUDUSD']
     impact_color = ['greenFont','redFont','blackFont']
+    order_operation = False
+    vote_decision = 0
+    strength = 0# 0 is neutral 1 is strong and 2 is weak
 
-    try:
-        print(len(news))
-        for i in range(len(news)):
-            target_news = news[i].find_all("td")
-            
-            t_currency = str(target_news[1]).split(' ')
-            t_currency = t_currency[-1].replace("</td>","")
-            print(t_currency)
+    hour = 0
+    min = 0
+    # Get the current local time
+    now = datetime.datetime.now()
+    # Extract hour and minute
+    hour = now.hour
+    min = now.minute
 
+    print("current time:",hour,":",min)
+    
+    if hour > 19: break
+
+    print("Total number of news ",len(news))
+    for i in range(len(news)):
+        target_news = news[i].find_all("td")
+        
+        t_currency = str(target_news[1]).split(' ')
+        t_currency = t_currency[-1].replace("</td>","")
+        print(t_currency)
+        if t_currency == "USD":
             t_impact = str(target_news[2]).split("High Volatility Expected")
             if len(t_impact) <= 1 : 
-                print("This news dooesn't have the impact i am looking for") 
+                #print("This news dooesn't have the impact i am looking for") 
+                pass
             else: 
-                print("News with high impact detected for ", t_currency)
+                #print("News with high impact detected for ", t_currency)
                 t_time = str(target_news[0]).replace('<td class="first left time js-time" title="">','')
                 t_time = t_time.replace("</td>",'')
+                d_time = t_time.split(":")
+                print(d_time)
+                if len(d_time) <= 1:
+                    if len(t_time.split("min")) > 1:   
+                        print(t_time, "Please wait as actual value will soon be released")
+                else:
+                    if hour < 10:
+                        if "0"+str(hour) == d_time[0]:
+                            if "0"+str(min) == d_time[1]:
+                                pass
+                            elif str(min) == d_time[1]:
+                                pass
+                            else:
+                                print("Please wait on some minutes")
+                                print(t_time)
+                        pass
+                    else:
+                        if str(hour) == d_time[0]:
+                            if "0"+str(min) == d_time[1]:
+                                pass
+                            elif str(min) == d_time[1]:
+                                pass
+                            else:
+                                print("Please wait on some minutes")
+                                print(t_time)
+                        pass
 
-                t_time = t_time.split(":")
-                print(t_time)
-                t_hour = 0
-                t_min = 0 
-
-                print("currency time ",t_hour,":",t_min)
-
-                # Get the current local time
-                now = datetime.datetime.now()
-
-                # Extract hour and minute
-                hour = now.hour
-                min = now.minute
-
-                print("current time:",hour,":",min)
+                
+                
                 '''
                 for target in currency:
                     if target == t_currency:
@@ -88,7 +116,11 @@ while True:
                             print("The news market is not active")
                     break
                 '''
-    except Exception as e:
-        print(e)
+
+    if(i == len(news) - 1 and order_operation):
+        #Monitor and Calculate type of trade and also pull out after 5 mins count
+        pass
     time.sleep(20)
+
+
 
